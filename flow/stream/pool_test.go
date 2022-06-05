5ddb55100d551814b58e5d86de6fd95f8751c2c0
Poletansky Viktor <poletansky@gmail.com>
2022-06-05 17:09:51 +0300

    test

--- flow/stream/pool_test.go
+++ flow/stream/pool_test.go
@@ -1,18 +1,25 @@
 package stream
 
-// func TestPool(t *testing.T) {
-// 	sleepTasks := Map(nats(), func(id int) func() int {
-// 		return func() int {
-// 			time.Sleep(10 * time.Millisecond)
-// 			return id
-// 		}
-// 	})
-// 	sleepTasks100 := Take(sleepTasks, 100)
-// 	pool := NewPool[func() int](10)
-// 	sleepResults := pool(sleepTasks100)
-// 	resultStream := Map(sleepResults, func(f func() int) int { return f() })
-// 	results := CollectToSlice(resultStream)
-// 	start := time.Now()
-// 	assert.Equal(t, 100, slice.SetSize(slice.ToSet(results)))
-// 	assert.WithinDuration(t, start, time.Now(), 200*time.Millisecond)
-// }
+import (
+	"testing"
+	"time"
+
+	"github.com/rprtr258/vk-utils/flow/slice"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPool(t *testing.T) {
+	sleepTasks := Map(nats(), func(id int) func() int {
+		return func() int {
+			time.Sleep(10 * time.Millisecond)
+			return id
+		}
+	})
+	sleepTasks100 := Take(sleepTasks, 100)
+	pool := NewPool[int](10)
+	resultStream := pool(sleepTasks100)
+	results := CollectToSlice(resultStream)
+	start := time.Now()
+	assert.Equal(t, 100, slice.SetSize(slice.ToSet(results)))
+	assert.WithinDuration(t, start, time.Now(), 150*time.Millisecond)
+}
